fd8369bb9e132656a3b314e3e6910027cb7a13fe
shiouyan <black00731@gmail.com>
2020-01-23 00:55:48 +0800

    +app.css / app.js

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,7 +9,7 @@
             <nav class="menuzord js-primary-navigation" role="navigation" aria-label="Primary Navigation">
 
                 <!--logo start-->
-                <a href="index.html" class="logo-brand">
+                <a href="/" class="logo-brand">
                     @if ($page=='index')
                         <img class="retina" src="assets/img/logo-dark.png" alt="Massive">
                     @else
