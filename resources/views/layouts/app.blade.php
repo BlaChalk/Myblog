8b56b377c9170a5781d1a9ac07e16b6d67bf98a5
shiouyan <black00731@gmail.com>
2020-01-22 19:21:51 +0800

    update index route +layouts blade +index view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   @include('layouts.head')
+</head>
+
+<body>
+    @include('layouts.preloader')
+
+    <div class="wrapper">
+        @include('layouts.header')
+        @yield('hero')
+        @yield('content')
+        @include('layouts.footer')
+    </div>
+
+    @include('layouts.js')
+</body>
+
+</html>
