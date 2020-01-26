593d1082d487d75b5fbd54ba345c9caf400c389b
shiouyan <black00731@gmail.com>
2020-01-26 17:20:53 +0800

    +Post Controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use RuntimeException;
+
+class PostController extends Controller
+{
+    public function admin()
+    {
+        $posts = Post::all();
+        return view('posts.admin', ['posts' => $posts]);
+    }
+
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $post = new Post;
+        $post->fill($request->all());
+        $post->save();
+
+        return redirect('/posts');
+    }
+}
