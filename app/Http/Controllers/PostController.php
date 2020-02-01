dc1f47f52bbe3c3184c46e0e6cab0642429400ed
shiouyan <black00731@gmail.com>
2020-02-02 02:10:36 +0800

    + category @single post page + _sidebar subview

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -75,8 +75,11 @@ class PostController extends Controller
     {
         if(Auth::check())
             return view('/posts.showByAdmin', ['post' => $post]);
-        else
-            return view('/posts.show', ['post' => $post]);
+        else{
+            $categories = Category::all();
+            return view('/posts.show', ['post' => $post, 'categories' => $categories]);
+        }
+
     }
 
     /**
